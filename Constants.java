96db221527547becbd36bb88db0665486bdb80ef
Zhang, Jason <jzhang636@gmail.com>
2019-10-02 16:42:27 -0400

    Implemented TwitterBot class.

---
+++ Constants.java
@@ -0,0 +1,12 @@
+public class Constants {
+   public final static int NUM_TWEET_FIELDS = 5;
+   public final static String SENTIM_POSITIVE = "positive";
+   public final static String SENTIM_NEGATIVE = "negative";
+   public final static String SENTIM_NEUTRAL = "neutral";
+   
+   public final static String TOPIC_NONE = "none";
+   
+   public final static String RESPONSE_AGREE = "I agree.";
+   public final static String RESPONSE_DISAGREE = "I disagree.";
+   public final static String RESPONSE_NOCOMMENTS = "No comments.";
+}
