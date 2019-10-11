56f6e94461cd26c55f8a64417b05222a6081e284
Jeff Zhang <jz1766american.edu>
2019-10-11 19:41:23 -0400

    Update Tweet.java

--- Tweet.java
+++ Tweet.java
@@ -4,7 +4,12 @@ public class Tweet{
    private String ID;
    private String date;
    private String text;
-   
-   
 
+   public Tweet(String[] fields ) {
+      this.topic = fields[0];
+      this.sentiment = fields[1];
+      this.ID = fields[2];
+      this.date = fields[3];
+      this.text = fields[4]
+   
 }
