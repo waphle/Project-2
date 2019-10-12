18b1dc3372c30e36d0fecc3fcbc899061f0d5e7d
Jeff Zhang <jz1766american.edu>
2019-10-12 01:21:27 -0400

    Update TwitterBot.java

--- TwitterBot.java
+++ TwitterBot.java
@@ -3,12 +3,21 @@ import java.util.Random;
 import java.util.Date;
 
 public class TwitterBot {
-   // Section name here
+   // Attributes
    private String topic;
    private String sentiment;
    private float irreTweetProbability;
    private static Random rand = new Random();
    private boolean guessMade;
    
-   
+   // Constructors
+   private TwitterBot() {
+      this.topic = Constants.TOPIC_NONE;
+      this.sentiment = Constants.SENTIM_NEUTRAL;
+      this.irreTweetProbability = 0.0f;
+      this.guessMade = false;
    }
+   
+   private TwitterBot(String topic
+     
+}
