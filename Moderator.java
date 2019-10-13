83cb84c7779b8189e8fff5ce2b20a8b06f959b94
Jeff Zhang <jz1766american.edu>
2019-10-13 18:02:08 -0400

    Update Moderator.java

--- Moderator.java
+++ Moderator.java
@@ -2,7 +2,33 @@ import java.util.Scanner;
 import java.util.Vector;
 
 public class Moderator {
+   private static final int defaultNumBots = 100;
+   private static final int defaultNumSpams = 1000;
+   private static final float defaultBotRespCorrectness = 0.50f; // 50% use
+   
+   // Topic start indece in the tweet backlog
+   private static final int[] topicStartIndece = { 0,         // "Apple"
+                                                   1142,      // "Google"
+                                                   2459,      // "Microsoft"
+                                                   3824       // "Twitter"
+                                                 };                                                  
+   
+   public static void main(String[] args) throws Exception {
+   
+      java.io.File file = new java.io.File("full-corpus.csv");
+      Scanner fileInput = new Scanner(file, "UTF-8");
+      Scanner consoleInput = new Scanner(System.in); 
 
-
+      // Load all tweets from the data file into a backlog (vector)
+      Vector tweetBacklog = new Vector();
+      int count = 0;
+      while (fileInput.hasNext()) {
+         String line = fileInput.nextLine();
+         
+         // Drop the first line that consists of the field names
+         if (count == 0) {
+            count++;
+            continue;
+         }
 
 }
